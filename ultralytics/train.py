0cdf474584121c18022d152c825bef6f84721c19
Nightwalkx <74856680+xi-jiajun@users.noreply.github.com>
2023-03-31 22:00:03 -0400

    update data path

--- ultralytics/train.py
+++ ultralytics/train.py
@@ -2,12 +2,10 @@ from ultralytics import YOLO
 
 def main():
     # Load a model
-    #model = YOLO('yolov8n.yaml')  # build a new model from YAML
     model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
-    #model = YOLO('yolov8n.yaml').load('yolov8n.pt')  # build from YAML and transfer weights
 
     # Train the model
-    model.train(data='../datasets/data.yaml', epochs=10, imgsz=640)
+    model.train(data='./datasets/data.yaml', epochs=10, imgsz=640)
 
 if __name__ == "__main__":
     main()
